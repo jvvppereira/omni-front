608a21eddde0882de560055fcbddcd0a2ecb8453
João Vítor <joao.paes@philips.com>
2022-11-28 22:29:26 -0300

    force update file

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.OMNI_BACK || 'https://omni-back.herokuapp.com/';
+const baseURL = process.env.OMNI_BACK || 'https://omni-back.herokuapp.com/'; 
 
 const api = axios.create({
   baseURL
